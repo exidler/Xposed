8dcacebef46d43924b9b7afb27a8b77c5d818cc8
Oleg <xx.exis.git.oleg@kot.ms>
2013-09-11 14:20:22 +0400

    version 30.1

---
+++ install.bat
@@ -0,0 +1,8 @@
+adb shell "su -c 'mount -o remount,rw /system'"
+adb push app_process_xposed_sdk18 /data/local/tmp/
+
+adb shell "su -c 'rm /system/bin/app_process; cp /data/local/tmp/app_process_xposed_sdk18 /system/bin/app_process'"
+adb shell "su -c 'chmod 0755 /system/bin/app_process'"
+adb shell "su -c 'chown 0:2000 /data/local/tmp/app_process'"
+adb shell "su -c 'rm /data/local/tmp/app_process_xposed_sdk18; sync'"
+pause
