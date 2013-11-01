9e8e6a5a29ce5648e4c6026e09d07d58118bcffb
Oleg <xx.exis.git.oleg@kot.ms>
2013-11-01 15:49:32 +0400

    merge with revo/v42 done

--- install.bat
+++ install.bat
@@ -3,6 +3,6 @@ adb push app_process_xposed_sdk18 /data/local/tmp/
 
 adb shell "su -c 'rm /system/bin/app_process; cp /data/local/tmp/app_process_xposed_sdk18 /system/bin/app_process'"
 adb shell "su -c 'chmod 0755 /system/bin/app_process'"
-adb shell "su -c 'chown 0:2000 /data/local/tmp/app_process'"
+adb shell "su -c 'chown 0:1000 /system/bin/app_process'"
 adb shell "su -c 'rm /data/local/tmp/app_process_xposed_sdk18; sync'"
 pause
